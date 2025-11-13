9455158a4e305050e7eca33daa5f2c41219e9f1d
Matthias Kalms <mkalms@bccfe.ca>
2025-11-13 13:21:16 -0800

    Add full punctuation to Semi Serif

--- src/rich_typography/fonts/semiserif.py
+++ src/rich_typography/fonts/semiserif.py
@@ -36,22 +36,25 @@ DIGITS = Glyphs(
     string.digits,
 )
 
-EXTRA = Glyphs(
+PUNCTUATION = Glyphs(
     """
-    ╷ ╭╮    ╱ ╲    ╷             
-    │ ╭╯   ╱   ╲   │ ┼┼       ╭─╮
-    │ │   ╱     ╲  │ ┼┼ ╶╴    │╭┤
-╵ · · ·  ╱       ╲ ╵       ╶╴ │╰╯
-                              ╰─╯
+╷ ╷╷    ╭┼╮ ◯  ╱ ╭╮╷ ╷ ╭ ╮                   ╱      ╱    ╲  ╭╮     ┌╴ ╲    ╶┐ ╱╲    ╲ ╭╴ ╷ ╶╮    
+│    ┼┼ ╰┼╮   ╱  ├─┼   │ │                  ╱  · · ╱  ╶╴  ╲ ╭╯ ╭─╮ │   ╲    │         ┼  │  ┼ ╭╮ 
+│    ┼┼   │  ╱   │ │   │ │ ╶╳╴ ╶┼╴ ╷ ╶╴    ╱   · ╷ ╲  ╶╴  ╱ │  │╭┤ │    ╲   │         │  │  │  ╰╯
+·       └┼╯ ╱  ◯ ╰╯╰   ╰ ╯         ╵    · ╱      ╵  ╲    ╱  ·  │╰╯ └╴    ╲ ╶┙    ╶╴   ╰╴ ╵ ╶╯    
+                                                               ╰─╯                               
 """,
-    ",.!?/\|#-_@",
+    string.punctuation,
 )
 
-SEMISERIF = Font("Semi Serif", UPPER | LOWER | DIGITS | EXTRA)
+SEMISERIF = Font("Semi Serif", UPPER | LOWER | DIGITS | PUNCTUATION)
 
 
 if __name__ == "__main__":
     from rich.console import Console
+    from rich_typography.typography import Typography
 
     console = Console()
-    console.print(SEMISERIF)
+    console.print(Typography("\"The quick brown fox jumps over the lazy dog,\" which contains every letter of the alphabet. ",SEMISERIF))
+    
+    console.print(Typography("Tom & Jerry ",SEMISERIF))
