0865eae6e5c1abf451e20f9f5eeb7794edef728a
Matthias Kalms <mtkalms@gmail.com>
2025-12-10 13:12:20 -0800

    Test: Add utilities for comparing resulting markup

--- tests/test_typography.py
+++ tests/test_typography.py
@@ -1,13 +1,23 @@
 import io
+from typing import Optional
+
 from assets.fonts import OVERLAP
 from rich.console import Console, RenderableType
+from rich.text import Text
 
 from rich_typography.typography import Typography
 
-def render(renderable: RenderableType) -> str:
-    console = Console(file=io.StringIO(), legacy_windows=False, width=256)
+
+def render(renderable: RenderableType, width: Optional[int] = 256) -> str:
+    file = io.StringIO()
+    console = Console(file=file, legacy_windows=False, width=width, record=True)
     console.print(renderable, no_wrap=True)
-    return console.file.getvalue()
+    return console.export_text(styles=True)
+
+
+def assert_markup(renderable: RenderableType, markup: str) -> None:
+    assert render(renderable) == render(Text.from_markup(markup))
+
 
 def test_overlap() -> None:
     expected = "\n".join([
