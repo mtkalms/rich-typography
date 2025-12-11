31db865115ab28f31c67ca7c58fa430508b7b133
Matthias Kalms <mtkalms@gmail.com>
2025-12-10 19:28:34 -0800

    Format files

--- tests/test_typography.py
+++ tests/test_typography.py
@@ -20,31 +20,28 @@ def assert_markup(renderable: RenderableType, markup: str) -> None:
 
 
 def test_overlap() -> None:
-    expected = (
-        "\n".join(
-            [
-                "╭╮   ╷  ",
-                "┼╭╮┌╮┼╷╷",
-                "││││ │││",
-                "╵╰╯╵ ╰╰┤",
-                "     ╰─╯",
-            ]
-        )
+    expected = "\n".join(
+        [
+            "╭╮   ╷  ",
+            "┼╭╮┌╮┼╷╷",
+            "││││ │││",
+            "╵╰╯╵ ╰╰┤",
+            "     ╰─╯",
+        ]
     )
     assert_markup(Typography("forty", font=OVERLAP), expected)
 
+
 def test_overlap_styles() -> None:
     markup = "[red on blue]f[/red on blue][purple on green]ort[/purple on green][red on blue]y[/red on blue]"
-    expected = (
-        "\n".join(
-            [
-                "[red on blue]╭[/red on blue][red on green]╮[/red on green][purple on green]   ╷[/purple on green][red on blue]  [/red on blue]",
-                "[red on blue]┼[/red on blue][purple on green]╭╮┌╮┼[/purple on green][red on blue]╷╷[/red on blue]",
-                "[red on blue]│[/red on blue][purple on green]│││ │[/purple on green][red on blue]││[/red on blue]",
-                "[red on blue]╵[/red on blue][purple on green]╰╯╵ ╰[/purple on green][red on blue]╰┤[/red on blue]",
-                "[red on blue] [/red on blue][purple on green]    [/purple on green][red on green]╰[/red on green][red on blue]─╯[/red on blue]",
-            ]
-        )
+    expected = "\n".join(
+        [
+            "[red on blue]╭[/red on blue][red on green]╮[/red on green][purple on green]   ╷[/purple on green][red on blue]  [/red on blue]",
+            "[red on blue]┼[/red on blue][purple on green]╭╮┌╮┼[/purple on green][red on blue]╷╷[/red on blue]",
+            "[red on blue]│[/red on blue][purple on green]│││ │[/purple on green][red on blue]││[/red on blue]",
+            "[red on blue]╵[/red on blue][purple on green]╰╯╵ ╰[/purple on green][red on blue]╰┤[/red on blue]",
+            "[red on blue] [/red on blue][purple on green]    [/purple on green][red on green]╰[/red on green][red on blue]─╯[/red on blue]",
+        ]
     )
     result = Typography.from_text(Text.from_markup(markup), font=OVERLAP)
     assert_markup(result, expected)
