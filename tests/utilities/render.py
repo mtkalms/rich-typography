22c99c261a259187cced2be1adcc03c4abe280d6
Matthias Kalms <mtkalms@gmail.com>
2025-12-12 11:15:25 -0800

    Test: Add justify option to render_ansi

--- tests/utilities/render.py
+++ tests/utilities/render.py
@@ -1,15 +1,16 @@
 import io
 from typing import Optional
 
-from rich.console import Console, RenderableType
+from rich.console import Console, RenderableType, JustifyMethod
 
 
 def render_ansi(
     renderable: RenderableType,
     width: Optional[int] = 80,
     no_wrap: Optional[bool] = True,
+    justify: JustifyMethod = "default",
 ) -> str:
     file = io.StringIO()
     console = Console(file=file, legacy_windows=False, width=width, record=True)
-    console.print(renderable, no_wrap=no_wrap)
+    console.print(renderable, no_wrap=no_wrap, justify=justify)
     return console.export_text(styles=True)
