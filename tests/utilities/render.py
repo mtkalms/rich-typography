4f1744c0c4486d0a235fac9a3056084361aa712e
Matthias Kalms <mtkalms@gmail.com>
2025-12-12 10:18:16 -0800

    Test: Add wrap option to render_ansi

--- tests/utilities/render.py
+++ tests/utilities/render.py
@@ -4,8 +4,12 @@ from typing import Optional
 from rich.console import Console, RenderableType
 
 
-def render_ansi(renderable: RenderableType, width: Optional[int] = 80) -> str:
+def render_ansi(
+    renderable: RenderableType,
+    width: Optional[int] = 80,
+    no_wrap: Optional[bool] = True,
+) -> str:
     file = io.StringIO()
     console = Console(file=file, legacy_windows=False, width=width, record=True)
-    console.print(renderable, no_wrap=True)
+    console.print(renderable, no_wrap=no_wrap)
     return console.export_text(styles=True)
