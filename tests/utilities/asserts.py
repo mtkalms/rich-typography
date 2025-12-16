ba428ebf02682ee7959e45529935339aeae4903b
Matthias Kalms <mtkalms@gmail.com>
2025-12-16 07:34:26 -0800

    Test: Add preview option to assert_markup

--- tests/utilities/asserts.py
+++ tests/utilities/asserts.py
@@ -1,8 +1,18 @@
+from itertools import zip_longest
+from typing import Optional
 from rich.console import RenderableType
 from rich.text import Text
 
 from tests.utilities.render import render_ansi
 
 
-def assert_markup(renderable: RenderableType, markup: str) -> None:
-    assert render_ansi(renderable) == render_ansi(Text.from_markup(markup))
+def assert_markup(
+    renderable: RenderableType, markup: str, preview: Optional[bool] = False
+) -> None:
+    rendered_a = render_ansi(renderable)
+    rendered_b = render_ansi(Text.from_markup(markup))
+    if preview:
+        lines_a = rendered_a.splitlines()
+        lines_b = rendered_b.splitlines()
+        print("\n".join(a + "\t" + b for a, b in zip_longest(lines_a, lines_b)))
+    assert rendered_a == rendered_b
