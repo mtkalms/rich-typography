4f2557e5bf83f2bee3034996ba922e9444c2d96b
Matthias Kalms <mtkalms@gmail.com>
2026-01-01 16:03:01 -0800

    Test: Improve preview

--- tests/utilities/asserts.py
+++ tests/utilities/asserts.py
@@ -1,19 +1,30 @@
 from itertools import zip_longest
 from typing import Optional
-from rich.console import RenderableType
+from rich.console import RenderableType, JustifyMethod
 from rich.text import Text
 
 from tests.utilities.render import render_ansi
 
 
 def assert_markup(
-    renderable: RenderableType, markup: str, preview: Optional[bool] = False
+    renderable: RenderableType,
+    markup: str,
+    message: Optional[str] = None,
+    preview: Optional[bool] = False,
+    justify: JustifyMethod = "default",
 ) -> None:
-    rendered_a = render_ansi(renderable)
+    rendered_a = render_ansi(renderable, justify=justify)
     rendered_b = render_ansi(Text.from_markup(markup))
     if preview:
         lines_a = rendered_a.splitlines()
         lines_b = rendered_b.splitlines()
+        # Get max line length (without ANSI codes)
+        max_len = max(len(Text.from_ansi(a)) for a in lines_a)
         print()
-        print("\n".join(a + "\t" + b for a, b in zip_longest(lines_a, lines_b)))
-    assert rendered_a == rendered_b
+        print(
+            "\n".join(
+                f'"{a}"'.ljust(max_len + 2) + f'\t"{b}"'
+                for a, b in zip_longest(lines_a, lines_b)
+            )
+        )
+    assert rendered_a == rendered_b, message
