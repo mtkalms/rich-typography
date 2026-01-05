1e0956d94056917d4213243581e694dd7c46d789
Matthias Kalms <mtkalms@gmail.com>
2026-01-04 17:12:17 -0800

    Test: Add tests for Glyphs

---
+++ tests/test_glyphs.py
@@ -0,0 +1,172 @@
+import pytest
+from rich_typography.glyphs import Glyphs
+from tests.utilities.markup import MarkupResult
+
+
+def test_simple_glyphs() -> None:
+    glyphs = Glyphs(
+        "abc",
+        "   ╷    ",
+        "┌╮ ├╮ ╭┐",
+        "╭┤ ││ │ ",
+        "╰┘ └╯ ╰╴",
+        "        ",
+    )
+    assert [
+        "  ",
+        "┌╮",
+        "╭┤",
+        "╰┘",
+        "  ",
+    ] == glyphs.get("a")
+    assert [
+        "╷ ",
+        "├╮",
+        "││",
+        "└╯",
+        "  ",
+    ] == glyphs.get("b")
+    assert [
+        "  ",
+        "╭┐",
+        "│ ",
+        "╰╴",
+        "  ",
+    ] == glyphs.get("c")
+    assert glyphs.get("d") is None
+
+
+def test_line_length_missmatch() -> None:
+    with pytest.raises(ValueError):
+        Glyphs(
+            "abc",
+            "   ╷",
+            "┌╮ ├╮ ╭┐",
+            "╭┤ ││ │ ",
+            "╰┘ └╯ ╰╴",
+            "        ",
+        )
+
+
+def test_char_glyph_missmatch() -> None:
+    with pytest.raises(ValueError):
+        Glyphs(
+            "abcd",
+            "   ╷    ",
+            "┌╮ ├╮ ╭┐",
+            "╭┤ ││ │ ",
+            "╰┘ └╯ ╰╴",
+            "        ",
+        )
+
+
+def test_glyph_union() -> None:
+    glyph_ab = Glyphs(
+        "ab",
+        "   ╷ ",
+        "┌╮ ├╮",
+        "╭┤ ││",
+        "╰┘ └╯",
+        "     ",
+    )
+    glyph_c = Glyphs(
+        "c",
+        "  ",
+        "╭┐",
+        "│ ",
+        "╰╴",
+        "  ",
+    )
+    glyphs = glyph_ab | glyph_c
+    assert [
+        "  ",
+        "┌╮",
+        "╭┤",
+        "╰┘",
+        "  ",
+    ] == glyphs.get("a")
+    assert [
+        "╷ ",
+        "├╮",
+        "││",
+        "└╯",
+        "  ",
+    ] == glyphs.get("b")
+    assert [
+        "  ",
+        "╭┐",
+        "│ ",
+        "╰╴",
+        "  ",
+    ] == glyphs.get("c")
+
+
+def test_glyph_union_height_missmatch() -> None:
+    glyph_ab = Glyphs(
+        "ab",
+        "   ╷ ",
+        "┌╮ ├╮",
+        "╭┤ ││",
+        "╰┘ └╯",
+        "     ",
+    )
+    glyph_c = Glyphs(
+        "c",
+        "╭┐",
+        "│ ",
+        "╰╴",
+        "  ",
+    )
+    with pytest.raises(ValueError):
+        glyph_ab | glyph_c
+
+
+def test_glyph_union_unsupported_type() -> None:
+    glyph_ab = Glyphs(
+        "ab",
+        "   ╷ ",
+        "┌╮ ├╮",
+        "╭┤ ││",
+        "╰┘ └╯",
+        "     ",
+    )
+    glyph_c = [
+        "  ",
+        "╭┐",
+        "│ ",
+        "╰╴",
+        "  ",
+    ]
+    with pytest.raises(TypeError):
+        glyph_ab | glyph_c
+
+
+def test_str() -> None:
+    glyphs = Glyphs(
+        "abc",
+        "   ╷    ",
+        "┌╮ ├╮ ╭┐",
+        "╭┤ ││ │ ",
+        "╰┘ └╯ ╰╴",
+        "        ",
+    )
+    assert MarkupResult(
+        "a  b  c ",
+        "   ╷    ",
+        "┌╮ ├╮ ╭┐",
+        "╭┤ ││ │ ",
+        "╰┘ └╯ ╰╴",
+        "        ",
+    ) == str(glyphs)
+
+
+def test_repr() -> None:
+    glyphs = Glyphs(
+        "abc",
+        "   ╷    ",
+        "┌╮ ├╮ ╭┐",
+        "╭┤ ││ │ ",
+        "╰┘ └╯ ╰╴",
+        "        ",
+    )
+    assert "<Glyphs: a b c>" == glyphs.__repr__()
