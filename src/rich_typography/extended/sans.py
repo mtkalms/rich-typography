3f78ddd24f3d8aec37598c384fe9bf0913da75fa
Matthias Kalms <mtkalms@gmail.com>
2026-01-13 18:14:21 -0800

    Fonts: First extended sans sketch

---
+++ src/rich_typography/extended/sans.py
@@ -0,0 +1,77 @@
+import string
+
+from rich_typography.fonts import Font
+from rich_typography.glyphs import Glyphs
+
+
+_upper = Glyphs(
+    string.ascii_uppercase,
+    "𜸚𜸟𜸤 𜸛𜸟𜸤 𜸚𜸟𜸤 𜸛𜸟𜸤 𜸛𜸟𜸥 𜸛𜸟𜸥 𜸚𜸟𜸤 𜸜 𜸜 𜸜  𜸞𜸧 𜸜 𜸜 𜸜   𜸝𜸢 𜸬𜸦 𜸝𜸢𜸜 𜸚𜸟𜸤 𜸛𜸟𜸤 𜸚𜸟𜸤 𜸛𜸟𜸤 𜸚𜸟𜸤 𜸞𜸟𜸠𜸟𜸥 𜸜 𜸜 𜸜𜸜 𜸜  𜸜 𜸜𜸜 𜸜𜸜 𜸞𜸤",
+    "𜸨𜸟𜸶 𜸨𜸟𜸷 𜸩 𜸼 𜸩 𜸩 𜸨𜸟  𜸨𜸟  𜸩 𜸼 𜸨𜸟𜸶 𜸩   𜸩 𜸨𜸟𜸷 𜸩   𜸩𜸫𜹁𜸻𜸩 𜸩𜸫𜸹 𜸩 𜸩 𜸩 𜸩 𜸩 𜸩 𜸨𜸟𜸷 𜸾𜸟𜸤   𜸩   𜸩 𜸩 𜸩𜸩 𜸩  𜸩 𜸮𜸷 𜸩𜸩 𜸚𜹃",
+    "𜸩 𜸩 𜸩 𜸩 𜸩   𜸩 𜸩 𜸩   𜸩   𜸩𜸞𜸧 𜸩 𜸩 𜸩 𜸜 𜸩 𜸩 𜸩 𜸩   𜸩   𜸩 𜸩 𜸩 𜸩 𜸩 𜸨𜸟𜹃 𜸩 𜸩 𜸩 𜸩   𜸩   𜸩   𜸩 𜸩 𜸩𜸩 𜸩𜸬𜸢𜸩 𜸩𜸩 𜸩𜸩 𜸩 ",
+    "𜸼 𜸼 𜸽𜸟𜹃 𜸾𜸟𜹃 𜸽𜸟𜹃 𜸽𜸟𜸥 𜸼   𜸾𜸟𜹃 𜸼 𜸼 𜸼 𜸾𜸟𜹃 𜸼 𜸼 𜸽𜸟𜸥 𜸼   𜸼 𜸼 𜸼 𜸾𜸟𜹃 𜸼   𜸾𜸟𜹅 𜸼 𜸼 𜸾𜸟𜹃   𜸼   𜸾𜸟𜹃 𜸾𜹄 𜸿𜸻𜸫𜹆 𜸼𜸼 𜸾𜸶 𜸾𜸥",
+    "                                                                                                 𜸾𜹃   ",
+)
+
+_lower = Glyphs(
+    string.ascii_lowercase,
+    "    𜸜         𜸜     𜸚𜸟𜸤     𜸜   𜸣   𜸣 𜸜   𜸜                               𜸜                       ",
+    "𜸚𜸟𜸤 𜸨𜸟𜸤 𜸚𜸟𜸤 𜸚𜸟𜸶 𜸚𜸟𜸤 𜸺𜸥  𜸚𜸟𜸧 𜸨𜸟𜸤 𜸜   𜸜 𜸩 𜸜 𜸩  𜸛𜸟𜸠𜸟𜸤 𜸛𜸟𜸤 𜸚𜸟𜸤 𜸛𜸟𜸤 𜸚𜸟𜸤 𜸛𜸤 𜸚𜸟𜸤 𜸺𜸥 𜸜 𜸜 𜸜 𜸜 𜸜  𜸜 𜸜𜸜 𜸜𜸜 𜸞𜸤",
+    "𜸚𜸟𜸶 𜸩 𜸩 𜸩   𜸩 𜸩 𜸨𜸟𜹄 𜸩   𜸩 𜸩 𜸩 𜸩 𜸩   𜸩 𜸨𜸟𜸷 𜸩  𜸩 𜸩 𜸩 𜸩 𜸩 𜸩 𜸩 𜸩 𜸩 𜸩 𜸩 𜸩  𜸾𜸟𜸤 𜸩  𜸩 𜸩 𜸩 𜸩 𜸩𜸬𜸢𜸩 𜸮𜸷 𜸩𜸩 𜸚𜹃",
+    "𜸾𜸟𜹃 𜸽𜸟𜹃 𜸾𜸟𜹃 𜸾𜸟𜹄 𜸾𜸟𜹃 𜸼   𜸾𜸟𜸶 𜸼 𜸼 𜸼   𜸩 𜸼 𜸼 𜸾𜸥 𜸼 𜸼 𜸼 𜸼 𜸼 𜸾𜸟𜹃 𜸨𜸟𜹃 𜸾𜸟𜸶 𜸼  𜸾𜸟𜹃 𜸾𜸟 𜸾𜸟𜹃 𜸫𜹁𜸻 𜸿𜸻𜸫𜹆 𜸼𜸼 𜸾𜸶 𜸾𜸥",
+    "                        𜸾𜸟𜹃       𜸾𜸟𜹃                      𜸼     𜸼                           𜸾𜹃   ",
+)
+
+_digits = Glyphs(
+    string.digits,
+    "𜸚𜸟𜸤 𜸜 𜸚𜸟𜸤 𜸚𜸟𜸤 𜸜 𜸜 𜸛𜸟𜸥 𜸚𜸟𜸥 𜸞𜸟𜸧 𜸚𜸟𜸤 𜸚𜸟𜸤",
+    "𜸩𜸢𜸩 𜸩 𜸚𜸟𜹃  𜸟𜸷 𜸾𜸟𜸶 𜸽𜸟𜸤 𜸨𜸟𜸤 𜸚𜸟𜹃 𜸮𜸟𜸷 𜸾𜸟𜸶",
+    "𜸩𜸫𜸹 𜸩 𜸩   𜸜 𜸩   𜸩 𜸜 𜸩 𜸩 𜸩 𜸩   𜸩 𜸩   𜸩",
+    "𜸾𜸟𜹃 𜸼 𜸽𜸟𜸥 𜸾𜸟𜹃   𜸼 𜸾𜸟𜹃 𜸾𜸟𜹃 𜸼   𜸾𜸟𜹃 𜸾𜸟𜹃",
+    "                                     ",
+)
+
+_punctuation = Glyphs(
+    string.punctuation,
+    "𜸜 𜸜𜸜    𜸚𜸺𜸤 𜸚𜸤   𜸚𜸤𜸜 𜸜 𜸚 𜸤                    ╱      ╱    ╲  𜸚𜸤 𜸚𜸟𜸤 𜸛𜸥 ╲    𜸞𜸧 ╱╲    ╲ 𜸚𜸥 𜸜 𜸞𜸤    ",
+    "𜸩    𜸺𜸺 𜸾𜸺𜸤 𜸾𜹃╱  𜸮𜸟𜸺   𜸩 𜸩 𜸪𜸲𜸸 𜸞𜸺𜸥   𜸞𜸟𜸥     ╱  𜹍   ╱  ══  ╲ 𜸚𜹃 𜸩𜸚𜸶 𜸩   ╲    𜸩         𜸺  𜸩  𜸺 𜸚𜸤 ",
+    "𜸩    𜸺𜸺  𜸩𜸩  ╱𜸚𜸤 𜸩 𜸩   𜸩 𜸩                  ╱   𜹍 𜹍 ╲      ╱ 𜸩  𜸩𜸾𜹃 𜸩    ╲   𜸩         𜸩  𜸩  𜸩  𜸾𜹃",
+    "𜹍       𜸾𜸺𜹃   𜸾𜹃 𜸾𜹃𜸾   𜸾 𜹃         𜸼     𜹍 ╱      𜸼  ╲    ╱  𜹍  𜸾𜸟𜹃 𜸽𜸥    ╲ 𜸞𜹄    𜸞𜸥   𜸾𜸥 𜸼 𜸞𜹃    ",
+    "                                                                                                  ",
+)
+
+_ligatures = Glyphs(
+    list(
+        "re ra ri rj ro ru fb ff fh fi fj fk fl ft ffb fff ffh ffi ffj ffk ffl fft Th Ti Tj".split()
+    ),
+    "         .  .           𜸚𜸧  𜸚𜸚𜸤 𜸚𜸧  𜸚𜸤 𜸚𜸤 𜸚𜸧  𜸚𜸧 𜸚𜸧 𜸚𜸚𜸧  𜸚𜸚𜸚𜸤 𜸚𜸚𜸧  𜸚𜸚𜸤 𜸚𜸚𜸤 𜸚𜸚𜸧  𜸚𜸚𜸧 𜸚𜸚𜸧 𜸞𜸟𜸠𜸟𜸧   𜸞𜸠𜸧 𜸞𜸠𜸧",
+    "𜸛𜸠𜸤 𜸛𜸤𜸤 𜸛𜸧 𜸛𜸧 𜸛𜸟𜸠𜸟𜸤 𜸛𜸧𜸜 𜸺𜸨𜸤 𜸺𜸺  𜸺𜸨𜸤 𜸺𜸧 𜸺𜸧 𜸺𜸩𜸜 𜸺𜸩 𜸺𜸺 𜸺𜸺𜸨𜸤 𜸺𜸺𜸺  𜸺𜸺𜸨𜸤 𜸺𜸺𜸧 𜸺𜸺𜸧 𜸺𜸺𜸩𜸜 𜸺𜸺𜸩 𜸺𜸺𜸺   𜸩 𜸨𜸟𜸤  𜸩𜸜  𜸩𜸜",
+    "𜸩𜸨𜹄 𜸩𜸚𜸶 𜸩𜸩 𜸩𜸩 𜸩 𜸩 𜸩 𜸩𜸩𜸩 𜸩𜸩𜸩 𜸩𜸩  𜸩𜸩𜸩 𜸩𜸩 𜸩𜸩 𜸩𜸨𜸷 𜸩𜸩 𜸩𜸩 𜸩𜸩𜸩𜸩 𜸩𜸩𜸩  𜸩𜸩𜸩𜸩 𜸩𜸩𜸩 𜸩𜸩𜸩 𜸩𜸩𜸨𜸷 𜸩𜸩𜸩 𜸩𜸩𜸩   𜸩 𜸩 𜸩  𜸩𜸩  𜸩𜸩",
+    "𜸼𜸾𜹃 𜸼𜸾𜹃 𜸼𜸼 𜸼𜸩 𜸼 𜸾𜸟𜹃 𜸼𜸾𜹃 𜸼𜸽𜹃 𜸼𜸼  𜸼𜸼𜸼 𜸼𜸼 𜸼𜸩 𜸼𜸼𜸼 𜸼𜸾 𜸼𜸾 𜸼𜸼𜸽𜹃 𜸼𜸼𜸼  𜸼𜸼𜸼𜸼 𜸼𜸼𜸼 𜸼𜸼𜸩 𜸼𜸼𜸼𜸼 𜸼𜸼𜸾 𜸼𜸼𜸾   𜸼 𜸼 𜸼  𜸼𜸼  𜸼𜸩",
+    "           𜸾𜹃                          𜸾𜹃                               𜸾𜹃                           𜸾𜹃",
+)
+
+SANS = Font(
+    "Sans",
+    _upper | _lower | _digits | _punctuation,
+    ligatures=_ligatures,
+    baseline=3,
+)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    from rich.console import Console
+    from rich_typography.typography import Typography
+
+    console = Console()
+    console.print(
+        Typography(
+            SANS.name,
+            font=SANS,
+        )
+    )
+    console.print(str(_upper))
+    console.print(str(_lower))
+    console.print(str(_digits))
+    console.print(str(_punctuation))
+    console.print(str(_ligatures))
